6ab0574b5ff5fee5b59f281abbf5f663699d53ab
Лаптев Алексей Юрьевич <a.laptev@urfu.ru>
2017-03-14 17:41:44 +0500

    ok

--- js/test.js
+++ js/test.js
@@ -1,1 +1,1 @@
-	$(document).ready(function(){$(".test1").animate({opacity: "0.1", right: "+=400px"}, 1200) });
+	$(document).ready(function(){$(".test1").animate({opacity: "0.1", right: "+=40px"}, 1200) });
